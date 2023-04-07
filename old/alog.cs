16788a91a34e9fb198b5970c5afc3f1607fb5e58
Austin_Xu <98461851+Austixu@users.noreply.github.com>
2023-04-07 22:36:26 +0800

    Add files via upload

---
+++ old/alog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+namespace AX
+{
+    class log
+    {
+        public enum Writetype
+        { Info, Error, Warn }
+        string time = DateTime.Now.ToString("HH:mm:ss");
+        string enter = "\r\n";
+        public void Create(string filepath)
+        {
+            if (filepath == string.Empty)
+            {
+                File.Create(System.Environment.CurrentDirectory + @"\" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + "log.alf");
+            }
+            else
+            {
+                File.Create(filepath).Close();
+                File.AppendAllText(filepath, "ALog,version 1.0.2" + enter);
+            }
+        }
+        public void Write(Writetype writetype,string filepath, string type,string contents)
+        {
+            if (writetype==Writetype.Info)
+            {
+                File.AppendAllText(filepath, "[" + time + "]" + "[" + type + "/INFO]" + contents+enter);
+            } else if(writetype==Writetype.Error)
+            {
+                File.AppendAllText(filepath, "[" + time + "]" + "[" + type + "/ERROR]" + contents + enter);
+            } else
+            {
+                File.AppendAllText(filepath, "[" + time + "]" + "[" + type + "/WARN]" + contents+enter); 
+            }
+        }
+    }
+}
